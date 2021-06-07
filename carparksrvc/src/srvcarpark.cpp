9ea485eb09b53c9f4732ee44751503be1714e894
Jacob Su <junnicesu@aliyun.com>
2021-06-07 12:57:31 +1200

    sj: add allow leaving ROS Node Service, not tested

--- carparksrvc/src/srvcarpark.cpp
+++ carparksrvc/src/srvcarpark.cpp
@@ -1,5 +1,6 @@
 #include "ros/ros.h"
 #include "carparksrvc/AllowParking.h"
+#include "carparksrvc/AllowLeaving.h"
 #include "carpark.h"
 
 //callback of allow parking
@@ -13,6 +14,16 @@ bool allow_parking(carparksrvc::AllowParking::Request &req,
     return true;
 }
 
+bool allow_leaving(carparksrvc::AllowLeaving::Request &req, 
+                    carparksrvc::AllowLeaving::Response &res )
+{
+    auto pCarPark_SI = carparksrvc::CarPark::GetInstance();
+    res.isAllowed = pCarPark_SI->ReqLeaving(req.isPaid, (carparksrvc::enumCarType)req.carType);  
+    ROS_INFO("request, cartype : %d,  %s", req.carType, req.isPaid? "Paid": "UnPaid");
+    ROS_INFO("response, allowed leaving : %s", res.isAllowed? "True": "False");    
+    return true;
+}
+
 carparksrvc::CarPark* carparksrvc::CarPark::pSI = nullptr;
 
 int main(int argc, char** argv)
@@ -20,6 +31,7 @@ int main(int argc, char** argv)
     ros::init(argc, argv, "carpark_service");
     ros::NodeHandle n;
     ros::ServiceServer service = n.advertiseService("carpark_req_parking", allow_parking);
+    ros::ServiceServer service2 = n.advertiseService("carpark_req_leaving", allow_leaving);
     ROS_INFO("Ready for car park service.");
     ros::spin();
     return 0;
--- carparksrvc/test/utest.cpp
+++ carparksrvc/test/utest.cpp
@@ -18,10 +18,15 @@ TEST(TestSuite, allowParking)
 {
     auto pCarPark_SI = carparksrvc::CarPark::GetInstance();
     ASSERT_TRUE (pCarPark_SI != nullptr);
-    for(int i=0; i<15; i++){
+    for(int i=0; i<15; i++){ //sj: 15 is the parking lot number for the bus, from the constructor, not from any configure.
         EXPECT_TRUE (pCarPark_SI->ReqParking(carparksrvc::enumCarType::bus));
     }
     EXPECT_FALSE (pCarPark_SI->ReqParking(carparksrvc::enumCarType::bus));
+
+    for(int i=0; i<100; i++){ //sj: 15 is the parking lot number for the car
+        EXPECT_TRUE (pCarPark_SI->ReqParking(carparksrvc::enumCarType::car));
+    }
+    EXPECT_FALSE (pCarPark_SI->ReqParking(carparksrvc::enumCarType::car));
 }
 
 TEST(TestSuite, allowleaving)
